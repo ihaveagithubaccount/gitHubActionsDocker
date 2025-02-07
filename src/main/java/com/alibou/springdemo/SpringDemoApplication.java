8e1f938e76be8ef4365500975c1042d9494768ea
elena <ematovska@gmail.com>
2025-02-07 22:47:52 +0100

    remove pom

--- src/main/java/com/alibou/springdemo/SpringDemoApplication.java
+++ src/main/java/com/alibou/springdemo/SpringDemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
 
